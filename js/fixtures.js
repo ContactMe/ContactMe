f847672d9e3155e60417ff8143c8fb26936c8f18
Jordan VanderZwaag <jvanderzwaag@enova.com>
2015-03-21 14:06:56 -0500

    added fixtures data

---
+++ js/fixtures.js
@@ -0,0 +1,14 @@
+
+window.FIXTURES = [{
+  id: 1,
+  name: 'George',
+  relationship: 'Brother'
+}, {
+  id: 2,
+  name: 'Hector',
+  relationship: 'Father'
+}, {
+  id: 3,
+  name: 'Margret',
+  relationship: 'Mother'
+}];
