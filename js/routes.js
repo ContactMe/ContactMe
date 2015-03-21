1f7b2f47e14157994bc86071941261ddb6ef4948
Jordan VanderZwaag <jvanderzwaag@enova.com>
2015-03-21 14:14:13 -0500

    add contact and contacts index routes

--- js/routes.js
+++ js/routes.js
@@ -10,3 +10,18 @@ ContactMe.ContactsRoute = Ember.Route.extend({
     return FIXTURES;
   }
 });
+
+ContactMe.ContactRoute = Ember.Route.extend({
+  model: function(params) {
+    return FIXTURES.findBy('id', parseInt(params.contact_id));
+  }
+});
+
+ContactMe.ContactsIndexRoute = Ember.Route.extend({
+  redirect: function(params) {
+    var contacts = this.modelFor('contacts');
+    if(!Ember.isEmpty(contacts)) {
+      this.replaceWith('contact', contacts.get('firstObject'));
+    }
+  }
+});
