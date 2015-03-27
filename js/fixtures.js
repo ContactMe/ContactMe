a19e1d0997d1a956f15d735962a5318f3f759980
Ben Fischer <bfischer@enova.com>
2015-03-27 13:26:43 -0500

    update models to include phone address and email

--- js/fixtures.js
+++ js/fixtures.js
@@ -1,14 +1,38 @@
 
 window.FIXTURES = [{
   id: 1,
-  name: 'George',
-  relationship: 'Brother'
+  name:         'Enova',
+  relationship: 'employer',
+  phoneNumber:  '3125684200',
+  email:        'contact@enova.com',
+  address: {
+    street: '200 W Jackson',
+    city:   'Chicago',
+    state:  'IL',
+    zip:    '60606'
+  }
 }, {
   id: 2,
   name: 'Hector',
-  relationship: 'Father'
+  relationship: 'Father',
+  phoneNumber: '8005882300',
+  address: {
+    city:   'Northlake',
+    state:  'IL'
+  }
 }, {
   id: 3,
   name: 'Margret',
-  relationship: 'Mother'
+  relationship: 'Mother',
+  phoneNumber: '8005882300',
+  address: {
+    city:   'Northlake',
+    state:  'IL'
+  }
+}, {
+  id: 4,
+  name: 'Glen Lerner',
+  relationship: 'lawyer',
+  phoneNumber: '2222222',
+  address: null
 }];
